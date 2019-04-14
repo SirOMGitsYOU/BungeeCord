268f9166511eb1d91f210c89ccf8d8bde2f10d43
BuildTools <unconfigured@null.spigotmc.org>
2019-04-14 17:19:08 +0200

    Merged branch 'master' from 'https://github.com/SpigotMC/BungeeCord'

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -31,7 +31,7 @@ public class ProtocolConstants
             "1.13.x"
     );
 
-    public static final List<Integer> SUPPORTED_VERSION_IDS = Arrays.asList( 
+    public static final List<Integer> SUPPORTED_VERSION_IDS = Arrays.asList(
             ProtocolConstants.MINECRAFT_1_7_2,
             ProtocolConstants.MINECRAFT_1_7_6,
             ProtocolConstants.MINECRAFT_1_8,
